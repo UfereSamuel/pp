69dc572a05d5ca3fd0a29d952b0e34d10eb3794e
Obeya Edward Obande <edwardobande36@gmail.com>
2017-05-23 17:52:43 +0100

    app credentials

--- configs/config.js
+++ configs/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    AppName: "WeMunize",
-    APP_ID: process.env.APP_ID || 'myAppId',
-    MASTER_KEY: process.env.MASTER_KEY || 'myMasterKey',
-    SERVER_URL: process.env.SERVER_URL || 'https://wemunize-dretnan.c9users.io/parse',
-    DASHBOARD_USER: process.env.DASHBOARD_USER || "retnan",
-    DASHBOARD_PASS: process.env.DASHBOARD_PASS || "yahweh",
+    AppName: "Ararocks",
+    APP_ID: process.env.APP_ID || 'ararocksappid',
+    MASTER_KEY: process.env.MASTER_KEY || 'ararocksmasterKey',
+    SERVER_URL: process.env.SERVER_URL || 'https://ararocks.herokuapp.com/parse',
+    DASHBOARD_USER: process.env.DASHBOARD_USER || "ararocks",
+    DASHBOARD_PASS: process.env.DASHBOARD_PASS || "ararocks",
 }
