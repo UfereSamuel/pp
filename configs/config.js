ce6533356dc7a4870a2b36f2a24b517571ee4e9e
Obeya Edward Obande <edwardobande36@gmail.com>
2017-05-27 14:32:58 +0100

    Update config.js

--- configs/config.js
+++ configs/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     AppName: "Ararocks",
-    APP_ID: process.env.APP_ID || 'ararocksappid',
-    MASTER_KEY: process.env.MASTER_KEY || 'ararocksmasterKey',
-    SERVER_URL: process.env.SERVER_URL || 'https://ararocks.herokuapp.com/parse',
-    DASHBOARD_USER: process.env.DASHBOARD_USER || "ararocks",
-    DASHBOARD_PASS: process.env.DASHBOARD_PASS || "ararocks",
+    APP_ID: process.env.APP_ID || 'ara-advisors',
+    MASTER_KEY: process.env.MASTER_KEY || 'ara-advisors',
+    SERVER_URL: process.env.SERVER_URL || 'https://ara-advisors.herokuapp.com/parse',
+    DASHBOARD_USER: process.env.DASHBOARD_USER || "ara-advisors",
+    DASHBOARD_PASS: process.env.DASHBOARD_PASS || "ara-advisors",
 }
