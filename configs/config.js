9fe624f97fdac327e276f1db7a1102a899e597b8
Sammy <uferesamuel@gmail.com>
2018-03-29 07:54:11 +0100

    changes

--- configs/config.js
+++ configs/config.js
@@ -2,8 +2,8 @@ module.exports = {
     APP_NAME: "samuel",
     APP_ID: process.env.APP_ID || 'tree-tracking',
     MASTER_KEY: process.env.MASTER_KEY || 'fellowship',
-    SERVER_URL: process.env.SERVER_URL || 'https://tree-trackers.herokuapp.com/parse',
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://heroku_p682jq5p:ra9n74deo1n36faeee66qlrtpu@ds135234.mlab.com:35234/heroku_p682jq5p',
+    SERVER_URL: process.env.SERVER_URL || 'https://tree-track.herokuapp.com/parse',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://heroku_ckcqrx5p:sqbb00rmj6u2ikf9mmuevh99ri@ds227119.mlab.com:27119/heroku_ckcqrx5p',
     DASHBOARD_USER: process.env.DASHBOARD_USER || "samuel",
     DASHBOARD_PASS: process.env.DASHBOARD_PASS || "father200",
 }
